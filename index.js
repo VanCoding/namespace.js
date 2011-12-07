86dfbf55d58e1742c233de0f965492d0a6e5ed0f
Patrik Stutz <patrik.stutz@gmail.com>
2011-12-07 06:19:57 -0600

    made it possible to 'use' at the beginning of files

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
-var fs = require("fs");
+﻿var fs = require("fs");
 function build(path,outputpath){
     var output = "(function(){"+
 		"function use(){"+
-			"if(!this.done){"+
-				"for(var i = 0; i < arguments.length; i++){"+
-					"var obj = arguments[i];"+
-					"for(var a in obj){"+
-						"this[a] = arguments[i][a];"+
-					"}"+
-				"}"+
-				"this.done = true;"+
+			"for(var i = 0; i < arguments.length; i++){"+
+				"var obj = arguments[i];"+
+				"this.__usings.push(obj);"+
 			"}"+
 		"};"+
 		"function m(s,p){"+
-			"h.push([s,p]);"+
+			"h.push(s);"+
+			"s.__usings = [p];"+
 			"return function(){use.apply(s,Array.prototype.slice.call(arguments,0));}"+
 		"}"+
 		"var h=[],c;";
 
 	var search = [{path:path,namespace:""}];
+    var files = [];
+    var namespaces = [];
 	while(search.length){
 		var nextsearch = [];
 		for(var i = 0; i < search.length; i++){
@@ -26,26 +24,37 @@ function build(path,outputpath){
 			var f = fs.readdirSync(s.path);
 			for(var j = 0; j < f.length; j++){
 				if(fs.statSync(s.path+f[j]).isFile()){
-					output += "c="+s.namespace.substr(0,s.namespace.length-1)+";"+
-					"c."+f[j].replace(".js","")+"=(function(){"+					
-						"with({}){"+
+					files.push("c="+s.namespace.substr(0,s.namespace.length-1)+";"+
+					"c."+f[j].replace(".js","")+"=new function(){"+	
+						"with(this){"+
 							"var use=m(this,c);"+
-							fs.readFileSync(s.path+f[j])+"\r\n"+
-							"return out;"+
+							"return (function(){"+
+								fs.readFileSync(s.path+f[j])+"\r\n"+
+								"return out;"+
+							"})();"+
 						"}"+
-					"})();";
+					"};");
 				}else{
-					output += "if(!this."+s.namespace+f[j]+"){"+s.namespace+f[j]+"={};}";
+					namespaces.push("if(!this."+s.namespace+f[j]+"){"+s.namespace+f[j]+"={};}");
 					nextsearch.push({path:s.path+f[j]+"/",namespace:s.namespace+f[j]+"."});
 				}
 			}
 		}
 		search = nextsearch;
 	}
+    for(var i = 0; i < namespaces.length; i++){
+        output += namespaces[i];
+    }
+    for(var i = 0; i < files.length; i++){
+        output += files[i];
+    }
 	output += 
 		"for(var i = 0; i < h.length; i++){"+
-			"for(var a in h[i][1]){"+
-				"h[i][0][a] = h[i][1][a];"+
+			"var u = h[i].__usings;"+
+			"for(var j = 0; j < u.length; j++){"+
+				"for(var a in u[j]){"+
+					"h[i][a] = u[j][a];"+
+				"}"+
 			"}"+
 		"}"+
 	"})();";
