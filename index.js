f72a5c984e363508f561901fa95b4d0db80ec702
Patrik Stutz <patrik.stutz@gmail.com>
2011-12-14 15:49:54 -0600

    now parsing each file to find usings. its much more elegant

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-﻿﻿var fs = require("fs");
+﻿﻿﻿var fs = require("fs");
 var modes = ["+","-"];
 
 function build(mode,path,outputpath){
-    var output = "(function(){"+
-		"function use(ns,name,f){"+
-			"var s = {usings:[ns]};"+
-			"use.h.push(s);"+
-			"ns[name]=f.call(s,function(){"+
-				"for(var i = 0; i < arguments.length; i++){"+
-					"s.usings.push(arguments[i]);"+
-				"}"+
-				"return ns;"+
-			"});"+
-		"};"+
-		"use.h=[];";
+    var output = "(function(){";
 
 	var search = [{path:path,namespace:""}];
 	path = [].concat(path);
@@ -37,15 +26,8 @@ function build(mode,path,outputpath){
 							break;
 						}
 					}
-					if(fs.statSync(s.path+f[j]).isFile()){						
-						files.push("use("+s.namespace.substr(0,s.namespace.length-1)+",'"+virtual.replace(".js","")+"',function(use){"+
-							"with(this){"+
-								"return (function(){"+
-									fs.readFileSync(s.path+real)+"\r\n"+
-									"return out;"+
-								"})();"+
-							"}"+
-						"});");
+					if(fs.statSync(s.path+f[j]).isFile()){		
+						files.push(buildFile(s.path+real,s.namespace.substr(0,s.namespace.length-1),virtual.substr(0,virtual.length-3)));
 					}else if(namespaces.indexOf(s.namespace+virtual) == -1){
 						namespaces.push("try{"+s.namespace+virtual+".x;}catch(e){"+s.namespace+virtual+"={};}");
 						nextsearch.push({path:s.path+real+"/",namespace:s.namespace+virtual+"."});
@@ -61,24 +43,89 @@ function build(mode,path,outputpath){
     for(var i = 0; i < files.length; i++){
         output += files[i];
     }
-	output += 
-		"(function(){"+
-			"for(var i = 0; i < use.h.length; i++){"+
-				"var u = use.h[i].usings;"+
-				"for(var j = 0; j < u.length; j++){"+
-					"for(var a in u[j]){"+
-						"use.h[i][a] = u[j][a];"+
-					"}"+
-				"}"+
-			"}"+
-		"})();"+
-	"})();";
+	output += "})();";
     
     if(outputpath){
         fs.writeFileSync(outputpath,output);
     }    
 	return output;
 }
+
+function buildFile(path,ns,name){
+	var out = "";
+	var f = (require("fs").readFileSync(path))+"";
+
+	for(var i = 0; i < f.length && i >= 0;){
+		switch(f.substr(i,2)){		
+			case "//":
+                for(;i < f.length; i++){
+                    if(f[i] == "\r" || f[i] == "\n"){
+                        i--;
+                        break;
+                    }
+                }
+				break;
+			case "/*":
+				i = f.indexOf("*/",i)+2;			
+				break;
+			default:
+                var c = f[i++];
+                out += c;
+				if(c == "'" || c == '"' || c == "/"){
+				    for(; i < f.length; i++){
+				        if(f[i] == "\\"){                            
+                            out += f[i];
+                            i++;
+				        }else if(f[i] == c){
+				            break;
+                        }else{
+                            out += f[i];
+                        }
+                    }
+                }
+				break;
+			
+		}
+	}
+
+	var usings = [];
+	var parts = out.split(";");
+	out = ns.length?"with("+ns+")":"";
+	for(var i = 0; i < parts.length; i++){
+		var p = parts[i].search(/\S/);
+		if(p == parts[i].indexOf("use")){
+			var l = parts[i].substr(p+3);
+		
+			var p2 = l.search(/\S/)
+			if(p2 >0 && p2 == l.search(/[a-zA-Z_$]/)){
+				l = l.substr(p2);
+				
+				var last = l.search(/\s/);
+				if(last>= 0){
+					l = l.substr(0,last);
+				}
+				usings.push(l);
+			}else{
+				parts = parts.slice(i);
+				break;
+			}
+		}else{
+			parts = parts.slice(i);
+			break;
+		}
+	}
+    
+	for(var i = 0; i < usings.length; i++){
+		out += "with("+usings[i]+")";
+	}
+    
+	out += "{"+ns+"."+name+"=(function(){";
+	out += parts.join(";");
+	out += ";return out;})();}";
+    
+	return out;
+}
+
 
 
 function buildAll(path,outpath){
